253e5771f2daa2c91ef1e70006d33848a8d77510
Tadaluuu <taidat97bn@gmail.com>
2022-11-27 17:48:40 +0700

    feat: init project

---
+++ components/Tutorial.vue
@@ -0,0 +1,10 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="text-2xl text-red-600">Apartments</div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+}
+</script>
