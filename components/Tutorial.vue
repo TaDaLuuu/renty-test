cac41dd637dcd28dc941bd73e7ac30ad7aeaa212
Tadaluuu <taidat97bn@gmail.com>
2022-11-27 22:04:13 +0700

    chore: wip

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -1,6 +1,6 @@
 <!-- Please remove this file from your project -->
 <template>
-  <div class="text-2xl text-red-600">Apartments</div>
+  <div class="text-2xl text-red-600">Tutorial</div>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
-  <Tutorial />
+  <Apartment />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Apartment from "../components/apartments/Apartment.vue"
 
 export default Vue.extend({
   name: 'IndexPage',
+  components: {
+    Apartment
+  },
 })
 </script>
